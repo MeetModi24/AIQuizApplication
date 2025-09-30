95a77c3f836e08b477397f75d9c1f1bc2ec5b33a
MeetModi24 <modimeet20@gmail.com>
2025-09-30 20:12:58 +0530

    neondb prisma setup

---
+++ src/lib/nextauth.ts
@@ -0,0 +1,8 @@
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions : NextAuthOptions={
+    session:{
+        strategy:"jwt"
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+}
